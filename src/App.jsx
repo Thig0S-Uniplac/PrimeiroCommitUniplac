c3226e5dece68266d6b58d474f5b2d6286e3ac93
Thiago Kovalski <thiagokovalski5@gmail.com>
2025-03-03 20:00:18 -0300

    Featire-ThiagoKovalski

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./assets/Components/Header/Header";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h2>Logoo</h2>
+      <nav>
+        <ul>
+          <li>
+            <a>Home</a>
+            <a>Seções</a>
+            <a>Contato</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default Header;
